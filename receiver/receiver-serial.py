9a9cec732a8b0fb6616ffe1adadc5f97fd139bae
masonfrykman <mfryk@uic.edu>
2026-04-26 12:46:23 -0500

    it works!

--- receiver/receiver-serial.py
+++ receiver/receiver-serial.py
@@ -10,13 +10,32 @@ from os import environ
 SERIAL_PATH = "/dev/cu.usbmodem94A99028C3401"
 if "T1_PSK" not in environ:
     print("T1_PSK must be a defined environment variable.")
-    raise SystemExit
+    raise SystemExit(1)
 
 PSK = environ['T1_PSK']
 
-needsReconnect = True
+# ---------
+# Load in fridge IDs
+fridges = []
+with open("fridges.txt", "r") as fridgeFile:
+    fridges = fridgeFile.readlines()
 
-def report(fridge: str, temp):
+i = 0
+while i < len(fridges):
+    fridges[i] = fridges[i].strip()
+    if(fridges[i] == "" or fridges[i].startswith("//")):
+        fridges.pop(i)
+        i -= 1
+    i += 1
+
+if len(fridges) < 1:
+    print("No fridges defined in fridges.txt")
+    raise SystemExit(1)
+
+def report(fridge: str, temp, printRep=False):
+    if printRep:
+        print(f"REPORT: {fridge} @ {temp}")
+    
     request = requests.post("https://t1.frykman.dev/api/report", headers={"X-Fridge": f"{fridge}", "X-PSK": PSK}, data=f"{temp}")
 
     if request.status_code == 403:
@@ -24,22 +43,55 @@ def report(fridge: str, temp):
     elif request.status_code != 200:
         print(f"Failed to report temperature (fridge: {fridge}, data: {temp}), received status code {request.status_code}.")
 
+# ---------
+# Meshtastic event handlers
+
+needsReconnect = True
+
 def onRecv(packet, interface):
     if "decoded" not in packet:
         return
 
     if "portnum" not in packet["decoded"]:
         return
+
+    if packet["decoded"]["portnum"] != 'PRIVATE_APP':
+        return
+
+    payload: bytes = packet["decoded"]["payload"]
     
-    if packet["decoded"]["portnum"] != 256:
+    if payload.startswith(b"TSNSR") == False:
+        print("Not a temperature reading message.")
         return
     
-    print(f"Received decoded packet with 256 port: ${packet}")
-    # TODO: need to receive data
+    data = payload.split(b"_")
+    if len(data) != 3:
+        print("Invalid packet data.")
+        return
+
+    # data[0]: 'TSNSR'
+    fridgeIDStr = data[1]
+    tempStr = data[2]
+
+    # convert values to integers
+    try:
+        global fridgeID
+        global temp
+        fridgeID = int(fridgeIDStr)
+        temp = int(tempStr)
+    except ValueError:
+        print("Recieved non-integer fields. Not reporting.")
+        return
+    
+    # Match fridge ID to fridge name
+    if(fridgeID < 0 or fridgeID >= len(fridges)):
+        print(f"Fridge ID of {fridgeID} is not in range [0, {len(fridges)})")
+        return
+    
+    fridge = fridges[fridgeID]
 
     # -- Send data to the webserver --
-    # TODO: replace this with the actual data received.
-    report("Star Farm", 99)
+    report(fridge, temp, printRep=True)
 
 def onDc(interface, topic=pub.AUTO_TOPIC):
     print("Connection lost! Will try to reconnect within the next 10 seconds.")
@@ -51,7 +103,7 @@ def onCxn(interface, topic=pub.AUTO_TOPIC):
     global needsReconnect
     needsReconnect = False
 
-pub.subscribe(onRecv, "meshtastic.receive")
+pub.subscribe(onRecv, "meshtastic.receive.data")
 pub.subscribe(onDc, "meshtastic.connection.lost")
 pub.subscribe(onCxn, "meshtastic.connection.established")
 
